19b10528d50a4b0ab766e2705aa5b719b12aaa88
David Garcia Orozco <David.GarOro@protonmail.com>
2023-05-05 12:46:38 -0600

    Add attractor neighborhoods

--- attractor_area.gd
+++ attractor_area.gd
@@ -1,19 +1,22 @@
 extends StaticBody3D
 
 @export var max_attractors: int = 500
+@export var destroy_dist: float = 0.4
+@export var segment_length: float = 0.2
 
-var num_attractors = 0
+var max_extent = Vector3(1,1,1)*-1.79769e308
+var min_extent = Vector3(1,1,1)*1.79769e308
+var max_dist = 0
 
 var attractor_scene = preload("res://attractor_point.tscn")
 
-@onready var max_extent = Vector3(1,1,1)*-1.79769e308
-@onready var min_extent = Vector3(1,1,1)*1.79769e308
 @onready var SpawnArea = $SpawnArea
 @onready var PointsParent = $PointsParent
 @onready var StartSegment = $SegmentsParent/Segment
 
 func _ready():
 	StartSegment.init($SegmentsParent.global_position)
+	# Find the extents of the Bounds
 	for point_local in SpawnArea.shape.points:
 		var point = SpawnArea.to_global(point_local)
 		
@@ -25,12 +28,28 @@ func _ready():
 		min_extent.y = minf(point.y,min_extent.y)
 		min_extent.z = minf(point.z,min_extent.z)
 	
-	while num_attractors < max_attractors:
+	var extent = abs(max_extent-min_extent)
+	
+	max_dist = maxf(maxf(extent.x,extent.y),extent.z)
+	
+	make_attractors(max_attractors)
+
+func make_attractors(num: int):
+	var num_made = 0
+	while num_made < num:
 		var attractor = attractor_scene.instantiate()
-		attractor.position = to_local(Vector3(randf_range(min_extent.x,max_extent.x),randf_range(min_extent.y,max_extent.y),randf_range(min_extent.z,max_extent.z)))
+		attractor.position = to_local(
+			Vector3(randf_range(min_extent.x,max_extent.x),
+					randf_range(min_extent.y,max_extent.y),
+					randf_range(min_extent.z,max_extent.z)))
 		PointsParent.add_child(attractor)
 		if(attractor.is_inside()):
-			num_attractors+=1
+			attractor.init(max_dist,destroy_dist)
+			num_made+=1
 		else:
 			attractor.free()
 
+func clear_attractors():
+	for n in PointsParent.get_children():
+		PointsParent.remove_child(n)
+		n.queue_free()
--- attractor_point.gd
+++ attractor_point.gd
@@ -1,5 +1,14 @@
 extends Node3D
 
+@onready var neighborhoods = $DistanceNeighborhoods
+
+func init(max_dist: float, destroy_dist: float):
+	var i = 0
+	for node in neighborhoods.get_children():
+		var d = (destroy_dist+(i*(max_dist-destroy_dist)/(neighborhoods.get_child_count()-1)))*2
+		node.scale=Vector3(d,d,d)
+		i+=1
+
 func is_inside()-> bool:
 	var params = PhysicsPointQueryParameters3D.new()
 	params.position = global_position
