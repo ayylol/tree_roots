d4c1b99e6b3e5485aef99eaaaa4b868b93c62ac8
David Garcia Orozco <David.GarOro@protonmail.com>
2023-05-12 14:42:49 -0600

    Add automatic extension for radial roots

--- radial_area.gd
+++ radial_area.gd
@@ -1,10 +1,11 @@
 extends Node3D
 
-@export var number_of_roots := 12
+@export var number_of_roots := 180
 @export var segment_length: float = 0.5
 @export var down_angle: float = PI/4
 @export var t: Transform3D = Transform3D()
 
+var extend = false
 var edge_segments = []
 
 @onready var StartSegment := $Segment
@@ -16,9 +17,15 @@ func _ready():
 		StartSegment.extend_dir = Vector3(cos(angle),-sin(down_angle),sin(angle));
 		edge_segments.push_back(StartSegment.extend(segment_length))
 
+func _physics_process(_delta):
+	if (extend):
+		extend_segments()
+
 func _unhandled_input(_event):
 	if(Input.is_action_just_pressed("StepSCA")):
 		extend_segments()
+	if(Input.is_action_just_pressed("ToggleSCA")):
+		extend = !extend
 	if(Input.is_action_just_pressed("Output")):
 		StartSegment.write_tree_to_file(t)
 
